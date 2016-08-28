7efd30a12da6241843ab135bbc823fb0a7a9a2c5
ayushgupta <rgblabsnstudio@gmail.com>
2016-08-28 12:55:10 +0530

    a

--- app/src/main/java/com/tripping/app/AddTripActivity.java
+++ app/src/main/java/com/tripping/app/AddTripActivity.java
@@ -18,23 +18,36 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.text.BreakIterator;
 
-public class AddTripActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, TextView.OnEditorActionListener {
+public class AddTripActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, TextView.OnEditorActionListener, View.OnFocusChangeListener {
     private Button mSearchRoutes;
     private Button mClearForm;
 
     private TextView mStartingPnt;
+    private LatLng mSourceLatLong;
     private TextView mDesitnationPnt;
+    private LatLng mDestinationLatLong;
 
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
 
     BreakIterator mLongitudeText;
     BreakIterator mLatitudeText;
+
+    private int PLACE_PICKER_REQUEST = 1;
+
+    private Place place;
+
+    private int textBoxToUse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,10 +66,12 @@ public class AddTripActivity extends AppCompatActivity implements View.OnClickLi
         mClearForm.setOnClickListener(this);
         mSearchRoutes.setOnClickListener(this);
 
-        mStartingPnt.setOnEditorActionListener(this);
-        mDesitnationPnt.setOnEditorActionListener(this);
+        //mStartingPnt.setOnEditorActionListener(this);
+        //mDesitnationPnt.setOnEditorActionListener(this);
         mStartingPnt.setOnClickListener(this);
         mDesitnationPnt.setOnClickListener(this);
+        //mStartingPnt.setOnFocusChangeListener(this);
+        //mDesitnationPnt.setOnFocusChangeListener(this);
 
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -81,19 +96,24 @@ public class AddTripActivity extends AppCompatActivity implements View.OnClickLi
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_srch_route:
-                startActivity(new Intent(this, TripRouteSelectActivity.class));
+                //startActivity(new Intent(this, TripRouteSelectActivity.class));
+
                 break;
             case R.id.btn_clear:
                 mDesitnationPnt.setText("");
                 mStartingPnt.setText("");
                 break;
             case R.id.starting_pnt_txt:
-                Snackbar.make(v, "Replace with your own action on Starting Point.", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(v, "Replace with your own action on Starting Point.", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();*/
+                textBoxToUse = R.id.starting_pnt_txt;
+                getPlaceToSelect();
                 break;
             case R.id.destination_pnt_txt:
-                Snackbar.make(v, "Replace with your own action on Destination Point.", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               /* Snackbar.make(v, "Replace with your own action on Destination Point.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                textBoxToUse = R.id.destination_pnt_txt;
+                getPlaceToSelect();
                 break;
         }
     }
@@ -146,5 +166,60 @@ public class AddTripActivity extends AppCompatActivity implements View.OnClickLi
     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 
         return false;
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+
+       getPlaceToSelect();
+        switch (v.getId()){
+            case R.id.starting_pnt_txt:
+               // if(hasFocus){
+                    textBoxToUse = R.id.starting_pnt_txt;
+                   // getPlaceToSelect();
+                  //  v.findViewById(R.id.starting_pnt_txt).setFocusable(false);
+                //}
+                break;
+            case R.id.destination_pnt_txt:
+                //if(hasFocus){
+                    textBoxToUse = R.id.destination_pnt_txt;
+                    //getPlaceToSelect();
+                  //  v.findViewById(R.id.destination_pnt_txt).setFocusable(false);
+                //}
+                break;
+        }
+
+    }
+    public void getPlaceToSelect(){
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                place = PlacePicker.getPlace(intent, this);
+            }
+            switch (textBoxToUse){
+                case R.id.starting_pnt_txt:
+                    mStartingPnt.setText(place.getName());
+                    mSourceLatLong = place.getLatLng();
+                    break;
+                case R.id.destination_pnt_txt:
+                    mDesitnationPnt.setText(place.getName());
+                    mDestinationLatLong = place.getLatLng();
+                    break;
+            }
+        }
+
     }
 }
